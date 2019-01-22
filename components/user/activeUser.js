b9452c68f3bf289f7b9c300df6ee34575092cc24
Denis Guimaraes <guimaraes.dev@gmail.com>
2019-01-22 13:59:42 +0100

    refactoring user data validation

--- components/user/activeUser.js
+++ components/user/activeUser.js
@@ -1,63 +1,16 @@
-// NPM import
-const validator = require('validator');
-
 // Local import
 const userError = require('./userError');
 const userDAL = require('./userDAL');
-const hash = require('../hash');
 
 // Code
-/**
- * Function dataValidator
- * @param {string} uuidUrl
- */
-const dataValidator = uuidUrl => {
-  let error = [];
-  // Validate uuidUrl
-  if (typeof uuidUrl === 'string') {
-    uuidUrl = uuidUrl.trim();
-    if (!validator.isUUID(uuidUrl, 4)) {
-      error.push('hash erreur');
-    }
-  } else {
-    error.push('mauvais format de hash');
-  }
-  // Return result
-  if (error.length <= 0) {
-    return uuidUrl;
-  } else {
-    return { error: [...error] };
-  }
-};
-
 /**
  * Function activeUser
  * @param {strind} uuidUrl
  */
-const activeUser = async uuidUrl => {
+const activeUser = async hashData => {
   let status;
   let body;
-  // Validate data
   try {
-    const validData = dataValidator(uuidUrl);
-    if (validData.error) {
-      // return status and body
-      status = 400;
-      body = { ...validData };
-      return { status, body };
-    }
-  } catch (error) {
-    // Log error
-    userError(error);
-    // Return status and data
-    status = 500;
-    body = { error: ['une erreur est survenue, veuillez réessayer !'] };
-    return { status, body };
-  }
-  // Active user
-  try {
-    // Get hash data
-    const hashData = await hash.findHash(uuidUrl);
     if (hashData.hash_action_id === 1) {
       await userDAL.activeUser(hashData.user_id);
       // Return status and data
--- components/user/userAPI.js
+++ components/user/userAPI.js
@@ -1,26 +1,56 @@
 // NPM import
 const express = require('express');
+const { checkSchema, validationResult } = require('express-validator/check');
 
 // Local import
+const userValidator = require('./userValidator');
 const createUser = require('./createUser');
+const email = require('../email');
+const hash = require('../hash');
 const activeUser = require('./activeUser');
 const connectUser = require('./connectUser');
+const jwt = require('../../jwt');
 
 // Code
 const router = express.Router();
 // Route for signup
-router.post('/signup', async (req, res) => {
+router.post('/signup', checkSchema(userValidator.signupSchema), async (req, res) => {
+  // Data validation
+  const error = await validationResult(req);
+  if (!error.isEmpty()) {
+    return res.status(422).send(error.array());
+  }
+  // Create user and send confirmation email
   const data = await createUser(req.body);
+  if (data.status === 200) {
+    email.confirmEmail(data.id);
+  }
   res.status(data.status).send(data.body);
 });
 // Route for active user
-router.get('/active/:hash', async (req, res) => {
-  const data = await activeUser(req.params.hash);
+router.get('/active/:hash', checkSchema(userValidator.activeSchema), async (req, res) => {
+  // Data validation
+  const error = await validationResult(req);
+  if (!error.isEmpty()) {
+    return res.status(422).send(error.array());
+  }
+  // Get hash data and active user
+  const hashData = await hash.findHash(req.params.hash);
+  const data = await activeUser(hashData);
   res.status(data.status).send(data.body);
 });
 // Route for signin
-router.post('/signin', async (req, res) => {
+router.post('/signin', checkSchema(userValidator.signinSchema), async (req, res) => {
+  // Data validation
+  const error = await validationResult(req);
+  if (!error.isEmpty()) {
+    return res.status(422).send(error.array());
+  }
+  // Connect user and return JWT
   const data = await connectUser(req.body);
+  if (data.status === 200) {
+    data.body.token = jwt.jwtSign(data.body);
+  }
   res.status(data.status).send(data.body);
 });
 
